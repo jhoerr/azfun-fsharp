e6c3399b7cbb7a07db63abfad6c44f3885d47e27
John Hoerr <jhoerr@gmail.com>
2018-09-10 17:11:15 -0400

    Add (empty) reducers and logging middleware

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,28 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import { createStore, applyMiddleware } from 'redux';
-// import loggingMiddleware from 'redux-logger';
-// import thunkMiddleware from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import loggingMiddleware from 'redux-logger';
 
 import 'rivet-uits/css/rivet.min.css';
 import App from './App';
-// import reducer from './modules';
-
-/* 
+import reducer from './store';
+ 
 const middleware = applyMiddleware(
-  loggingMiddleware,
-  thunkMiddleware
+  loggingMiddleware
+  // thunkMiddleware
 );
 const defaultState = {};
 const store = createStore(reducer, defaultState, middleware);
-*/
 
 ReactDOM.render(
-  // <Provider store={store}>
+  <Provider store={store}>
     <Router>
       <App />
-    </Router>,
-  // </Provider>
+    </Router>
+  </Provider>,
   document.getElementById('root'));
