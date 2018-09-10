5cbda510cbea3e0489702ae3624e61c23523d02f
John Hoerr <jhoerr@gmail.com>
2018-09-10 17:50:08 -0400

    Fix tests, add test for default routing

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,9 +1,21 @@
+import { mount } from 'enzyme';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  mount(
+    <Router>
+      <App />
+    </Router>
+  );
+});
+
+it('renders the home page by default', () => {
+  const cut = mount(
+    <Router>
+      <App />
+    </Router>
+  );
+  expect(cut.find("h1").text()).toBe("Home")
 });
